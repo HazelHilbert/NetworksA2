564832b1f76439b4acd7e2be1e69a88723002ce6
Hazel Hilbert <hazelhilbert@gmail.com>
2023-11-07 22:49:32 +0000

    containers know their networks

---
+++ constants.py
@@ -0,0 +1,22 @@
+NETWORKS = {
+    'edge1': ('172.20.1.255', 24),
+    'access1': ('10.20.10.255', 24),
+    'access2': ('10.20.20.255', 24),
+    'edge2': ('192.168.17.255', 24)
+}
+
+CONTAINERS = {
+    '5816e53d1d28': 'endpoint1',
+    'aa05304517ab': 'router1',
+    '8a2e750d2943': 'router2',
+    '19f13edd4ece': 'router3',
+    'c69437103918': 'endpoint2'
+}
+
+BROADCAST_NETWORKS = {
+    'endpoint1': 'edge1',
+    'router1': ('edge1', 'access1'),
+    'router2': ('access1', 'access2'),
+    'router3': ('access2', 'edge2'),
+    'endpoint2': 'edge2'
+}
--- node.py
+++ node.py
@@ -1,15 +1,35 @@
+import os
 import socket
 import time
 import uuid
 
+from constants import *
+
 
 class Node:
-    def __init__(self, address):
-        self.address = address
+    def __init__(self):
+        # generate 4-byte address
+        self.address = uuid.UUID(int=uuid.getnode()).bytes[-4:]
+        self.format_address = ':'.join(['{:02x}'.format(b) for b in self.address])
+
+        # initialize container name and networks the container is connected to
+        self.container_name = CONTAINERS[os.environ.get('HOSTNAME')]
+        self.connected_networks = BROADCAST_NETWORKS[self.container_name]
+        if isinstance(self.connected_networks, tuple):
+            self.broadcast_ip_port = tuple(NETWORKS[network] for network in self.connected_networks)
+        else:
+            self.broadcast_ip_port = NETWORKS[self.connected_networks]
+
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.socket.bind(('', 50000))
         self.buffer_size = 65536
+
+    def print_info(self):
+        print("Container: " + self.container_name)
+        print("Address: " + self.format_address)
+        print("Connected to networks: " + str(self.connected_networks))
+        print("Broadcast to these addresses " + str(self.broadcast_ip_port))
 
     def broadcast(self, payload):
         self.socket.sendto(payload, ('<broadcast>', 50000))
@@ -18,7 +38,8 @@ class Node:
         data, sender_address = self.socket.recvfrom(self.buffer_size)
         if sender_address != self.socket.getsockname():
             print(data.decode())
-            payload = ("Hi I am your edge " + str(self.address) + str(self.socket.getsockname()[0]) + str(self.socket.getsockname()[1])).encode()
+            payload = ("Hi I am your edge " + str(self.address) + str(self.socket.getsockname()[0]) + str(
+                self.socket.getsockname()[1])).encode()
             self.socket.sendto(payload, sender_address)
 
     def receive_edge_address(self):
@@ -32,25 +53,10 @@ class Node:
         return addresses
 
 
-class ForwardingTableEntry:
-    def __init__(self, destination, next_hop, timer):
-        self.destination = destination
-        self.next_hop = next_hop
-        self.timer = timer
-
-
 class Router(Node):
-    def __init__(self, address):
-        super().__init__(address)
+    def __init__(self):
+        super().__init__()
         self.forwarding_table = []
-        self.edges = []
-
-    def add_edge(self, address_port):
-        self.edges.append(address_port)
-
-    def add_edges(self, address_ports):
-        for addr in address_ports:
-            self.edges.append(addr)
 
     def add_entry_to_forwarding_table(self, destination, next_hop, timer):
         entry = ForwardingTableEntry(destination, next_hop, timer)
@@ -61,13 +67,12 @@ class Router(Node):
 
 
 class Endpoint(Node):
-    def __init__(self, address):
-        super().__init__(address)
-        self.gateway = None
-
-    def set_gateway(self, address_port):
-        self.gateway = address_port
+    def __init__(self):
+        super().__init__()
 
 
-
-
+class ForwardingTableEntry:
+    def __init__(self, destination, next_hop, timer):
+        self.destination = destination
+        self.next_hop = next_hop
+        self.timer = timer
